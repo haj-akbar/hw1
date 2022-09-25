250ee53aab8d32a866b8308369b706ec6f4f2fce
haj-akbar <akbar.agha1399@gmail.com>
2022-09-25 06:17:29 -0700

    fix bug ph6

--- haj_akbar_ph6.py
+++ haj_akbar_ph6.py
@@ -1,7 +1,7 @@
 #"calculation of the amount of profit and food"
 price_meal = float(input("please enter the price of your meal:"))
-Tax = ((9 * 100) / price_meal)
-Tip = ((18 * 100) / price_meal)
+Tax = ((9 * price_meal) / 100)
+Tip = ((18 * price_meal) / 100)
 Sum = (Tax + Tip + price_meal)
 Total = round(Sum , 2)
 print("you must pay",Total,"pound")
