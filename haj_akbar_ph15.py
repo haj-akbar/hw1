c5e94666073255f338ac54e778857e0e206970a3
haj-akbar <akbar.agha1399@gmail.com>
2022-09-29 06:20:44 -0700

    convert feet to yards,inches, and miles

---
+++ haj_akbar_ph15.py
@@ -0,0 +1,8 @@
+#convert feet to yards,inches, and miles
+feet = float(input("foot : "))
+miles = ( feet * 0.0001893939 )
+inches = ( feet * 12 )
+yards = ( feet * 0.3333333333 )
+print ( miles , "miles" )
+print ( inches , "inches" )
+print ( yards , "yards" )
