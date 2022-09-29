e36bbf0d888edb46abca2337f8604acdef4a727d
haj-akbar <akbar.agha1399@gmail.com>
2022-09-29 06:19:24 -0700

    convert feet to inches and centimeters!!

---
+++ haj_akbar_ph14.py
@@ -0,0 +1,6 @@
+#convert feet to inches and centimeters!!
+feet = float(input( "foot : " ))
+inch = ( feet * 12 )
+centi_meters = ( inch * 2.54 )
+print ( inch , "inch" )
+print ( centi_meters , "centi_meters" )
