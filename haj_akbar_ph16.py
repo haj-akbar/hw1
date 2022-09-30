adc0729e9d27d962788de4aac91a09f6fd5a21e8
haj-akbar <akbar.agha1399@gmail.com>
2022-09-30 05:15:05 -0700

    calculate the area and volume of a circle

---
+++ haj_akbar_ph16.py
@@ -0,0 +1,8 @@
+#calculate the area and volume of a circle!!
+from math import pi
+r = float(input("Enter the radius : "))
+area = ((( 4 / 3) * pi ) * (r ** 3))
+volume = ((4 * pi ) * (r ** 2))
+print( "area" , area )
+print( "volume" , volume) 
+
