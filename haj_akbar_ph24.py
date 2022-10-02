aa2b49e230c7e4dd0b092925f8ac7aa4d097bd97
haj-akbar <akbar.agha1399@gmail.com>
2022-10-02 11:55:35 -0700

    tested code

---
+++ haj_akbar_ph24.py
@@ -0,0 +1,5 @@
+#Convert days,hours and minutes to seconds!!
+days = float(input("enter the days : "))
+hours = float(input("enter the hours : "))
+minutes = float(input("enter the minutes : "))
+seconds = float(input("enter the seconds : "))
