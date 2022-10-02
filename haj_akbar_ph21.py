518997bcca6579510f3a74dade44b03e0dcae3a1
haj-akbar <akbar.agha1399@gmail.com>
2022-10-02 11:22:23 -0700

    Calculate the area of the triangle

---
+++ haj_akbar_ph21.py
@@ -0,0 +1,5 @@
+#Calculate the area of the triangle!!
+rule = float(input("Enter the rule : "))
+height = float(input("Enter the height : "))
+area = ((rule * height ) / 2)
+print("The area is equal to :" , area)
