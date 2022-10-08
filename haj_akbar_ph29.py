c3f3147fee84c7985a49edf01476f8e6045c655f
haj-akbar <akbar.agha1399@gmail.com>
2022-10-08 05:58:52 -0700

    convert celsius to kelvin and fahrenheit

---
+++ haj_akbar_ph29.py
@@ -0,0 +1,5 @@
+#convert celsius to kelvin and fahrenheit!!
+celsius = float(input("Enter celsius : "))
+fahrenheit = ((celsius * 9.5) + 32)
+kelvin = (celsius + 273.15)
+print("fahrenheit" ,fahrenheit , "kelvin" , kelvin )
