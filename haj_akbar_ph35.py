055293044b7c3f65a9e41f3b7ab2849ac8260cae
haj-akbar <akbar.agha1399@gmail.com>
2022-10-21 12:54:04 -0700

    strange question

---
+++ haj_akbar_ph35.py
@@ -0,0 +1,9 @@
+#strange question!!
+age_dog = float(input("enter the age dog : "))
+age_man = float(input("enter the age man : "))
+if age_dog > 10.5:
+    print("dont bite !!")
+elif (age_man / age_dog) >= 7 :
+    print("true")
+else :
+    print("false")    
