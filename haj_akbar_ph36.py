6d1518b71900bfa2f5da75ce6e57b246a01abc08
haj-akbar <akbar.agha1399@gmail.com>
2022-10-21 13:14:44 -0700

    voweld consonant-!!

---
+++ haj_akbar_ph36.py
@@ -0,0 +1,6 @@
+#voweld consonant-!!
+str_gang =input("enter the letter : ")
+if (str_gang == "a") or (str_gang == "e") or (str_gang =="i") or (str_gang =="o") or (str_gang =="u") :
+    print("vowel") 
+else :
+    print("consonant")
