6f8c23f3857c3259780a6bffc25d1d0ad1fe4187
haj-akbar <akbar.agha1399@gmail.com>
2022-11-18 11:37:27 -0800

    Identify the frequncy through the name of the bomb

---
+++ haj_akbar_ph42.py
@@ -0,0 +1,18 @@
+#Identify the frequncy through the name of the bomb!!
+bomb_name = input("enter your bomb name : ")
+if (bomb_name == "C4") :
+    print("261.63")
+elif (bomb_name == "D4") :
+    print("293.66")
+elif (bomb_name == "E4") :
+    print("329.63")
+elif (bomb_name == "F4") :
+    print("349.23")
+elif (bomb_name == "G4") :
+    print("392")
+elif (bomb_name == "A4") :
+    print("440")
+elif (bomb_name == 493.88) :
+    print("493.88 ")
+else :
+    print("Undefined")
