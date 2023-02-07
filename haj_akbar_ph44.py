6e5c33fad8c9ead6db5cf49722535fa19c8a4dd4
haj-akbar <akbar.agha1399@gmail.com>
2023-02-07 05:13:45 -0800

    Canadian holidays

---
+++ haj_akbar_ph44.py
@@ -0,0 +1,12 @@
+#Holidays in Canada!!
+month = input("Enter your desired month : ")
+day = int(input("Enter the day : "))
+if (month == "january") and (day == 1):
+    print("new years day")
+elif (month == "july") and (day == 1):   
+    print("canada day")
+elif (month == "december") and (day == 25):
+    print("christmas day")    
+else:
+    print("work day")    
+     
