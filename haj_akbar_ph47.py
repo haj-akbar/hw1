247f226392886f23a508dcc31b4d973a27e262a6
haj-akbar <akbar.agha1399@gmail.com>
2023-07-07 13:29:10 +0430

    zodiac sign

---
+++ haj_akbar_ph47.py
@@ -0,0 +1,27 @@
+#zodiac sign!!!
+season = input("enter season : ")
+day = int(input("enter day : "))
+if ((season == "december") and (day >= 22)) or ((season == "january") and (day <= 19)):
+    print("capricom")
+if ((season == "january") and (day >=18 )) or ((season == "february") and (day <= 20)):
+    print("aquarius")
+if ((season == "february") and (day >=19 )) or ((season == "march") and (day <= 20)):
+    print("pisces")
+if ((season == "march") and (day >=21 )) or ((season == "april") and (day <= 19)):
+    print("aries")
+if ((season == "april") and (day >=20 )) or ((season == "may") and (day <= 20)):
+    print("taurus")
+if ((season == "may") and (day >=21 )) or ((season == "june") and (day <=20)):
+    print("gemini")   
+if ((season == "june") and (day >=21 )) or ((season == "july") and (day <=22)):
+    print("cancer")       
+if ((season == "july") and (day >=23 )) or ((season == "august") and (day <=22)):
+    print("leo")
+if ((season == "august") and (day >=23 )) or ((season == "september") and (day <=22)):
+    print("virgo")         
+if ((season == "september") and (day >=23 )) or ((season == "october") and (day <=22)):
+    print("libra")
+if ((season == "october") and (day >=21 )) or ((season == "november") and (day <=20)):
+    print("scorpio")                       
+if ((season == "november") and (day >=22 )) or ((season == "december") and (day <=21)):
+    print("scorpio")                       
