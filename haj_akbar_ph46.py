6a3b07209eccfe1462dda80de746a9040edaff9a
haj-akbar <akbar.agha1399@gmail.com>
2023-07-15 04:11:40 +0430

    fix bug

--- haj_akbar_ph46.py
+++ haj_akbar_ph46.py
@@ -4,16 +4,20 @@ day = int(input("enter day : "))
 if (season == "march")  or (season == "april")  or (season == "may") or (season == "june"):
     if (day < 20 ) and (season == "march"):
         print("winter")
+        exit(0)
     if (season ==  "june") and ( day < 21):
          print("summer")
+         exit(0)
     print("spring")
 if (season ==  "july") or (season ==  "agust") or (season ==  "september"):
     if (season ==  "september") and (day > 22):
         print("fall")
+        exit(0)
     print("summer")
 if (season ==  "october") or (season ==  "novamber") or (season ==  "december"):
     if (season ==  "december") and (day > 21) : 
         print("witer")
+        exit(0)
     print("fall")
 if (season ==  "january") or (season ==  "february"):
     print("winter")
